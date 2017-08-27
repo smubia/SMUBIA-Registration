adcc28f2034f378f39c50e14db09bf0613c0ee8b
Michael Ong <minghao.ong.2014@sis.smu.edu.sg>
2017-08-27 18:23:36 +0800

    done with the front end, left with the backend

--- js/main.js
+++ js/main.js
@@ -1,12 +1,50 @@
-$(document).ready(function () {
-    console.log("ready!");
+$("#welcome").click(function () {
+    $("#error-page").hide("slide", {
+        direction: "right",
+    }, 1500);
+
+    $("#start-page").hide("slide", {
+        direction: "right",
+    }, 1500);
+
+    $("#welcome-page").show("slide", {
+        direction: "left"
+    }, 1500);
+
+    setTimeout(function () {
+        $("#welcome-page").hide("slide", {
+            direction: "right"
+        }, 1500);
+
+        $("#start-page").show("slide", {
+            direction: "left",
+        }, 1500);
+    }, 3000);
+
 });
 
-$( "#welcome" ).click(function() {
-  $("#welcome-page").show("slide", { direction: "left" }, 1000); 
-});
 
+$("#error").click(function () {
+    $("#welcome-page").hide("slide", {
+        direction: "right",
+    }, 1500);
 
-$( "#error" ).click(function() {
-    $("#error-page").show("slide", { direction: "left" }, 1000); 
+    $("#start-page").hide("slide", {
+        direction: "right",
+    }, 1500);
+
+    $("#error-page").show("slide", {
+        direction: "left"
+    }, 1500);
+
+    setTimeout(function () {
+        $("#error-page").hide("slide", {
+            direction: "right"
+        }, 1500);
+
+        $("#start-page").show("slide", {
+            direction: "left",
+        }, 1500);
+    }, 6000);
+
 });
