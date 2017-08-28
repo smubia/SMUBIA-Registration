4b26a80dea1c320cace79d162c568c4851b914ea
Michael Ong <minghao.ong.2014@sis.smu.edu.sg>
2017-08-28 13:58:38 +0800

    final push for front end and back end

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ Tools Used: **HTML, CSS, JavaScript**
 Creator: **Michael Ong Ming Hao** (minghao.ong.2014@sis.smu.edu.sg) <br/>
 Mobile: +65 8183 7009 <br/>
 Alt Email: minghao_3728@hotmail.com<br/>
-Live Visualisation URL: https://smubia.github.io/SMUBIA-VivaceAnalytics/
-Google Sheets File: 
+Live Visualisation URL: https://smubia.github.io/SMUBIA-VivaceAnalytics/ <br/>
+Google Sheets for Attendance Checking: https://docs.google.com/spreadsheets/d/1d0JRgd4Lo_DyglJZ_UURvx7COKzFnWSS6MuhLuwLEmY/edit#gid=0<br/>
+Google Sheet for all the NRIC Scanned and Timestamp: https://docs.google.com/spreadsheets/d/1jdYn0oa0DHkaarUL6tFSTzkvBZy68BlASUadjWj9geQ/edit#gid=0
--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
-$("#welcome").click(function () {
+var nricList = [];
+var nameList = [];
+
+$(document).ready(function () {
+    retrieveData();
+
+    setInterval(function () {
+        //$("#nric-control").focus();
+    }, 1000);
+});
+
+
+var retrieveData = function () {
+    $.ajax({
+        url: 'https://script.googleusercontent.com/macros/echo?user_content_key=vNfpxircLqNTqp3E0SL8n7FHWFMhvODoe9uX3Jeeujnoz5btETaVI3uWlQFUs_zY8sTd64dSdORm50m6HCOG5LuTTuPOr5O_OJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMWojr9NvTBuBLhyHCd5hHa1ZsYSbt7G4nMhEEDL32U4DxjO7V7yvmJPXJTBuCiTGh3rUPjpYM_V0PJJG7TIaKp8qKaos0Ar9PJpv1EzfNB2zG95ixg1Ju9KTaXqEycbTBHs_nfHv-wakO9Oe92mAFHq_uDj7qsXF0yz39XobvtEQQc09CumtDuA&lib=MbpKbbfePtAVndrs259dhPT7ROjQYJ8yx',
+        success: function (responseText) {
+            var data = responseText.AttendanceList;
+            console.log(data);
+            for (var i = 0; i < data.length; i++) {
+                nricList.push(data[i].NRIC);
+                nameList.push(data[i].NAME);
+            }
+        }
+    });
+}
+
+var welcomeFunction = function() {
     $("#error-page").hide("slide", {
         direction: "right",
     }, 1500);
@@ -20,11 +46,10 @@ $("#welcome").click(function () {
             direction: "left",
         }, 1500);
     }, 3000);
+};
 
-});
 
-
-$("#error").click(function () {
+var errorFunction = function() {
     $("#welcome-page").hide("slide", {
         direction: "right",
     }, 1500);
@@ -47,4 +72,23 @@ $("#error").click(function () {
         }, 1500);
     }, 6000);
 
+};
+
+// Contine to read the thing. If the length of it is 9 letter, then do a function
+$("#nric-control").keyup(function () {
+    //console.log($("#nric-control").val());
+    if ($("#nric-control").val().length == 9) {
+        sendStuff();
+        if (nricList.indexOf($("#nric-control").val()) == -1) {
+            errorFunction();
+        } else {
+            welcomeFunction();
+            $(".welcome-name").text(nameList[nricList.indexOf($("#nric-control").val())]);
+        }
+        $("#nric-control").val("");
+    }
 });
+
+var sendStuff = function () {
+    console.log("supposed to send somewhere.")
+}
