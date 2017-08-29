1c4893e07198b0d1a1b1f23ab8ee6480352691e3
Michael Ong <minghao.ong.2014@sis.smu.edu.sg>
2017-08-29 16:25:09 +0800

    mobile friendly + added validation for NRIC Scanning

--- js/main1.js
+++ js/main1.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     retrieveData();
 
     setInterval(function () {
-        //$("#nric-control").focus();
+        $("#nric-control").focus();
     }, 1000);
 });
 
@@ -15,9 +15,8 @@ var retrieveData = function () {
         url: 'https://script.googleusercontent.com/macros/echo?user_content_key=0amoNlIV6gc21BapdcZyeaMw40xsBtMyaLG3u-GrTSpnKg6IXMQZ7A_eSPCUCWbmIrkpmsAZG-NOe6xKyDrYF-rkZMF_vCWAOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMWojr9NvTBuBLhyHCd5hHa1ZsYSbt7G4nMhEEDL32U4DxjO7V7yvmJPXJTBuCiTGh3rUPjpYM_V0PJJG7TIaKp6myI78o9NC86WrdeeFbEqvTp3KCCDmpzBJIYT-ZdrtNoAKD_2nWLhB7mRe4EH6pyK_uDj7qsXF0yz39XobvtEQQc09CumtDuA&lib=MbpKbbfePtAVndrs259dhPT7ROjQYJ8yx',
         success: function (responseText) {
             var data = responseText.AttendanceList;
-            console.log(data);
             for (var i = 0; i < data.length; i++) {
-                nricList.push(data[i].NRIC);
+                nricList.push(data[i].NRIC.toUpperCase());
                 nameList.push(data[i].NAME);
             }
         }
@@ -38,6 +37,8 @@ var welcomeFunction = function () {
     }, 1500);
 
     setTimeout(function () {
+        $("#nric-control").val("");
+
         $("#welcome-page").hide("slide", {
             direction: "right"
         }, 1500);
@@ -47,7 +48,6 @@ var welcomeFunction = function () {
         }, 1500);
     }, 3000);
 };
-
 
 var errorFunction = function () {
     $("#welcome-page").hide("slide", {
@@ -63,6 +63,8 @@ var errorFunction = function () {
     }, 1500);
 
     setTimeout(function () {
+        $("#nric-control").val("");
+
         $("#error-page").hide("slide", {
             direction: "right"
         }, 1500);
@@ -79,13 +81,13 @@ $("#nric-control").keyup(function () {
     //console.log($("#nric-control").val());
     if ($("#nric-control").val().length == 9) {
         sendStuff();
-        if (nricList.indexOf($("#nric-control").val()) == -1) {
+        if (nricList.indexOf($("#nric-control").val().toUpperCase()) == -1) {
             errorFunction();
         } else {
             welcomeFunction();
             $(".welcome-name").text(nameList[nricList.indexOf($("#nric-control").val())]);
         }
-        $("#nric-control").val("");
+
     }
 });
 
@@ -93,8 +95,8 @@ var script_url = "https://script.google.com/macros/s/AKfycbwz5grRQJ-4Dij2WKqfnST
 
 // Make an AJAX call to Google Script
 function sendStuff() {
-    var name = $("#nric-control").val();
-    var url = script_url + "?name=" + name + "&action=insert";
+    var nric = $("#nric-control").val();
+    var url = script_url + "?name=" + nric + "&action=insert";
     var request = jQuery.ajax({
         crossDomain: true,
         url: url,
